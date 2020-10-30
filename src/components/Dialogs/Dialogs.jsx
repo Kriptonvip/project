e1938cad935fadcfd8e18c51de55041a5e356372
Kriptonvip <simenk@mail.ru>
2020-10-30 12:16:37 +0300

    commit6

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,38 +2,61 @@ import Dialog from './Dialog/Dialog';
 import s from './Dialogs.module.css';
 import { NavLink } from 'react-router-dom';
 
-const Dialogs = (props) => {
+const People = (props) => {
     return (
-    <div className={s.dialogs}>
-            <div className={s.dialogsItems}>Контакты
-            <div className={`${s.people} ${s.active}`}>
-                <NavLink to="/dialogs/1">Оля</NavLink>
-                </div>
-            <div className={s.people}>
-                 <NavLink to="/dialogs/2">Саша</NavLink>
-            </div> 
-            <div className={s.people}>
-                 <NavLink to="/dialogs/3">Егор</NavLink>
-            </div> 
-            <div className={s.people}>
-                 <NavLink to="/dialogs/4">Мама</NavLink>
-            </div> 
-            <div className={s.people}>
-                 <NavLink to="/dialogs/5">Папа</NavLink>
-            </div>      
-            </div>
-        <div className={s.dialog}>
-            <div className={s.headTitle}>Сообщения</div>
-            <p>Привет как дела?</p>
-            <p>Норм учу React</p>
-            <p>Когда на собес?</p>
-            <p>В декабре</p>
-            <p>Круто удачи!</p>
-            <p>Благодарю!</p>
-            <textarea></textarea>
-            <button>Add post</button>
+        <div className={s.people}>
+            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
         </div>
-    </div>
+    );
+}
+const Message = (props) => {
+    return (
+        <div className={s.message}>
+            <p>{props.message}</p>
+        </div>
+    )
+}
+
+const Dialogs = (props) => {
+    let peoplesData = [
+        { id: 1, name: 'Оля' },
+        { id: 2, name: 'Егор' },
+        { id: 3, name: 'Рома' },
+        { id: 4, name: 'Рашид' },
+        { id: 5, name: 'Коля' },
+        { id: 6, name: 'Саша' }
+    ];
+    let messagesData = [
+        { id: 1, message: 'Привет как дела?' },
+        { id: 2, message: 'Норм учу React' },
+        { id: 3, message: 'Когда на собес?' },
+        { id: 4, message: 'В декабре' },
+        { id: 5, message: 'Круто удачи!' },
+        { id: 6, message: 'Благодарю!' }
+    ];
+    let peopleElements = peoplesData.map((p) => (<People name={p.name} id={p.id} />))
+    // let peopleElements = [
+    //     <People name={peopleData[0].name} id={peopleData[0].id} />,
+    //     <People name={peopleData[1].name} id={peopleData[1].id} />,
+    //     <People name={peopleData[2].name} id={peopleData[2].id} />,
+    //     <People name={peopleData[3].name} id={peopleData[3].id} />,
+    //     <People name={peopleData[4].name} id={peopleData[4].id} />,
+    //     <People name={peopleData[5].name} id={peopleData[5].id} />
+    // ]; 
+    let messageElements = messagesData.map((m) => (<Message message={m.message} />))
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>Контакты
+                {peopleElements}
+            </div>
+            <div className={s.dialog}>
+                <div className={s.headTitle}>Сообщения</div>
+                {messageElements}
+                <textarea></textarea>
+                <button>Add post</button>
+            </div>
+        </div>
     )
 }
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,13 +3,14 @@ import Post from './Post/Post';
 
 
 const MyPosts = () => {
+    let postsData = [
+        { id: 1, message: 'Hi how are you', likesCount: 12 },
+        { id: 2, message: 'Hi i`am ok', likesCount: 14  },
+        { id: 3, message: 'Let`s go to swim', likesCount: 19  },
+    ];
+    let postItem = postsData.map((p) => (<Post message={p.message} likesCount={p.likesCount} />))
     return <div className={s.posts}> My posts
-        <Post message='Hi how are you' likeCount='12'/>
-        <Post message="Hi i`am ok" likeCount='14'/>
-        <Post message="Let`s go to swim" likeCount='19'/>
-        <Post />
-        <Post />
-        
+        {postItem}    
         </div>
 }
 
