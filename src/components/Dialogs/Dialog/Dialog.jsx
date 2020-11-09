e81b0b57f366add432c21288edfa5225fb91c993
Kriptonvip <simenk@mail.ru>
2020-11-09 09:59:35 +0300

    commit12

---
+++ src/components/Dialogs/Dialog/Dialog.jsx
@@ -0,0 +1,11 @@
+import s from './Dialog.module.css';
+
+const Dialog = () => {
+    return ( <div>
+        <div>
+        </div>
+    </div>
+    )
+}
+
+export default Dialog;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,8 +2,6 @@ import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from './ProfileInfo/ProfileInfo'
 
 
-
-
 const Profile = (props) => {
     return <div>
         <ProfileInfo state={props.state} addPost={props.addPost} newPostText={props.state.newPostText} addNewSymbol={props.addNewSymbol}/>
